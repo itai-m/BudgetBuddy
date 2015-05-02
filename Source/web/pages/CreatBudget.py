42fedf5d41644e78be3c0d59234768c52bc9470b
Itai Michael <Itaimichael@gmail.com>
2015-05-02 16:39:04 +0300

    add CreatBudget page

---
+++ Source/web/pages/CreatBudget.py
@@ -0,0 +1,14 @@
+
+from google.appengine.ext.webapp import template
+import webapp2
+
+class IndexHandler(webapp2.RequestHandler):
+	def get(self):
+		template_params = {}
+		
+		html = template.render("web/templates/CreatBudget.html", template_params)
+		self.response.write(html)
+
+app = webapp2.WSGIApplication([
+	('/CreatBudget', IndexHandler)
+], debug=True)
