45e817ef45102f5e58f130a3b856c8084f30d609
Itai Michael <Itaimichael@gmail.com>
2015-05-02 14:05:39 +0300

    add first pyton file

---
+++ Source/web/pages/Home.py
@@ -0,0 +1,14 @@
+
+from google.appengine.ext.webapp import template
+import webapp2
+
+class IndexHandler(webapp2.RequestHandler):
+	def get(self):
+		template_params = {}
+		
+		html = template.render("web/templates/top_navbar.html", template_params)
+		self.response.write(html)
+
+app = webapp2.WSGIApplication([
+	('/', IndexHandler)
+], debug=True)
