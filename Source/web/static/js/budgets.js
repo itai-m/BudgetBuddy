4456e2b73a0cdf17d3f6a5ebdcf5b71527e70093
Liran BG <liranbg@gmail.com>
2015-05-10 17:32:39 +0300

    css and design fixes

--- Source/web/static/js/budgets.js
+++ Source/web/static/js/budgets.js
@@ -1,9 +1,4 @@
-$(".button").click(function () {
-  $("#sForm").toggleClass("open");   
-});
-
 $(".controlTd").click(function () {
-  $(this).children(".settingsIcons").toggleClass("display"); 
-  $(this).children(".settingsIcon").toggleClass("openIcon"); 
+  $(this).children(".settingsIcons").toggleClass("display");
+  $(this).children(".settingsIcon").toggleClass("openIcon");
 });
-
