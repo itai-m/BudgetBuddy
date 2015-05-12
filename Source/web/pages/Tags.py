dd71fcd4d81642941329d6b0c6cfc37a77af6608
Nadav <Nadav2051@gmail.com>
2015-05-12 10:00:28 +0300

    Changed casing for the path

--- Source/web/pages/Tags.py
+++ Source/web/pages/Tags.py
@@ -5,7 +5,7 @@ import webapp2
 class IndexHandler(webapp2.RequestHandler):
     def get(self):
         template_params = {}
-        html = template.render("web/templates/Tags.html", template_params)
+        html = template.render("web/templates/tags.html", template_params)
         self.response.write(html)
 
 app = webapp2.WSGIApplication([('/Tags', IndexHandler)], debug=True)
