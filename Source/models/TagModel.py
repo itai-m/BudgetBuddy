a34a3ed6c98ed572a1cd86890d261dac4327897d
Liran BG <liranbg@gmail.com>
2015-05-22 12:20:39 +0300

    added tag model

--- Source/models/TagModel.py
+++ Source/models/TagModel.py
@@ -3,14 +3,14 @@ from google.appengine.ext import ndb
 class Tag(ndb.Model):
     description = ndb.StringProperty()
 
-
-    @staticmethod 
+    @staticmethod
     def getTag(tagKey):
-        return Tag.query(Tag.key == tagKey)
+        return Tag.query(Tag.key == tagKey).get()
 
     @staticmethod
     def addTag(tag):
         tag.put()
+        return tag.key.id()
     
     @staticmethod
     def removeTag(tag):
