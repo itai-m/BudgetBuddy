27d393efefb54db63e2310e4c654abafa140e43c
nadav2051 <nadav2051@gmail.com>
2015-05-27 16:33:39 +0300

    Added login validation for empty strings

--- Source/web/static/js/master_controls.js
+++ Source/web/static/js/master_controls.js
@@ -8,7 +8,13 @@ $(function() {  //this is jQuery's short notation for "fire all this when page i
 function submitLogin() {
 	var username = $('#loginUN').val();
 	var password = $('#loginPSW').val();
-	$.ajax({
+	if (username == null || username == "" || password == null || password == "")
+	{
+		alert("There appears to be a field missing from the form.");
+	}
+	else
+	{
+		$.ajax({
 		url:'/LoginCheck',
 		type:'GET',
 		dataType:'json',
@@ -25,5 +31,6 @@ function submitLogin() {
 			alert(xhr.responseText);
 			console.error(xhr, status, error);
 		}
-	});
+		});
+	}
 }
