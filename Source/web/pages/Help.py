fc70a58257aa08f4f8cf99ad56ca3217a3afcc53
Nadav <Nadav2051@gmail.com>
2015-05-30 15:16:50 +0300

    #25 Added the needed code to make the correct selection for the master page to display

--- Source/web/pages/Help.py
+++ Source/web/pages/Help.py
@@ -1,11 +1,24 @@
 from google.appengine.ext.webapp import template
 import webapp2
+from models.BudgeteerModel import Budgeteer
 
-
-class IndexHandler(webapp2.RequestHandler):
+class HelpHandler(webapp2.RequestHandler):
     def get(self):
         template_params = {}
+        loggedIn = True
+        if self.request.cookies.get('budgeteerIdToken'):
+            budgeteer = Budgeteer.getBudgeteerById(long(self.request.cookies.get('budgeteerIdToken')))
+            if not budgeteer:
+                loggedIn = False
+        else:
+                loggedIn = False
+        
+        if loggedIn:
+            template_params['base_template'] = "master_page.html"
+        else:
+            template_params['base_template'] = "guest_master.html"
+
         html = template.render("web/templates/Help.html", template_params)
         self.response.write(html)
 
-app = webapp2.WSGIApplication([('/Help', IndexHandler)], debug=True)
+app = webapp2.WSGIApplication([('/Help', HelpHandler)], debug=True)
