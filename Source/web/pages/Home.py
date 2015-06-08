4773c356fb7e018add336d8189ccd06fe0bc40d8
Liran BG <liranbg@gmail.com>
2015-06-08 19:39:15 +0300

    fixed missing filter error in home #22

--- Source/web/pages/Home.py
+++ Source/web/pages/Home.py
@@ -12,6 +12,7 @@ class IndexHandler(webapp2.RequestHandler):
         if not budgeteer:
             self.redirect('/Login')
             return
+        template.register_template_library('web.templatetags.filter_app')
         template_params = dict()
         template_params['userName'] = budgeteer.userName
         html = template.render("web/templates/master_page.html", template_params)
