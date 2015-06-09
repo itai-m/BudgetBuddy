a627f417bc920dcaf7d15dc8d57535a2842a3d9d
Rotem <rotemmm0@gmail.com>
2015-06-09 11:40:32 +0300

    #11 Finished the Edit Budget page

--- Source/web/pages/EditBudget.py
+++ Source/web/pages/EditBudget.py
@@ -3,9 +3,12 @@ import webapp2
 from models.BudgeteerModel import Budgeteer
 from models.BudgetModel import Budget
 from models.TagModel import Tag
+from models.EntryModel import Entry
+import datetime
+import json
 
 class EditBudgetHandler(webapp2.RequestHandler):
-    def get(self,budgetId):
+    def get(self,budget_id):
         if self.request.cookies.get('budgeteerIdToken'):
             budgeteer = Budgeteer.getBudgeteerById(long(self.request.cookies.get('budgeteerIdToken')))
         else:
@@ -17,29 +20,110 @@ class EditBudgetHandler(webapp2.RequestHandler):
 
         template.register_template_library('web.templatetags.filter_app')
         template_params = dict()
-        tagObjectList = Tag.getAllTags()
-        tagnamePairList = []
+        tag_object_list = Tag.getAllTags()
+        tag_name_pair_list = []
 
-        budget=Budget.getBudgetById(long(budgetId))
+        budget=Budget.getBudgetById(long(budget_id))
         budgetBudgeteersId = Budget.getAssociatedBudgeteersId(budget)
 
+        # Remove the "untagged" tag. the user should not be able to see it.
+        temp_object_list = []
+        for tag in tag_object_list:
+            if tag.description != "Untagged":
+                temp_object_list.append(tag)
+        tag_object_list = temp_object_list
 
-        if len(tagObjectList)%2 == 1:
-            tagObjectList.append(None)
-        for i in range (0,len(tagObjectList),2):
-            if tagObjectList[i+1] == None:
-                tagnamePairList.append([tagObjectList[i].description, None])
+        if len(tag_object_list)%2 == 1:
+            tag_object_list.append(None)
+        for i in range (0,len(tag_object_list),2):
+            if tag_object_list[i+1] == None:
+                tag_name_pair_list.append([tag_object_list[i].description, None])
             else:
-                tagnamePairList.append([tagObjectList[i].description, tagObjectList[i+1].description])
+                tag_name_pair_list.append([tag_object_list[i].description, tag_object_list[i+1].description])
 
+        temp_tag_list = Budget.getTagList(budget)
+        budget_tag_list = []
+        for tag in temp_tag_list:
+            budget_tag_list.append(tag.description)
         template.register_template_library('web.templatetags.filter_app')
         template_params['userName'] = budgeteer.userName
         template_params['budget'] = budget
         template_params['budgetName'] = budget.budgetName
-        template_params['tagnamePairList'] = tagnamePairList
-
+        template_params['tagnamePairList'] = tag_name_pair_list
+        template_params['checkedTags'] = budget_tag_list
+        template_params['budgetId'] = budget_id
 
         html = template.render("web/templates/EditBudget.html", template_params)
         self.response.write(html)
 
-app = webapp2.WSGIApplication([('/EditBudget/(.*)', EditBudgetHandler)], debug=True)
+
+class SubmitEditedBudgetHandler(webapp2.RequestHandler):
+    def get(self, budget_id):
+        if self.request.cookies.get('budgeteerIdToken'):
+            budgeteer = Budgeteer.getBudgeteerById(long(self.request.cookies.get('budgeteerIdToken')))
+            if not budgeteer:
+                self.redirect('/Login')
+                return
+        else:
+            self.redirect('/Login')
+            return
+        budget_name = self.request.get('budgetName')
+        tag_list = self.request.get('tagList')
+        participant_list = self.request.get('participantList')
+
+        budget = Budget.getBudgetById(long(budget_id))
+        print budget
+        budget.budgetName = budget_name
+        budget.creationDate = datetime.datetime.now()
+        budget.tagList = []
+
+        for participant in Budget.getAssociatedBudgeteersId(budget):
+            Budget.removeBudgeteerFromBudget(participant, budget)
+
+        # Add the Untagged entry to the budget.
+        budget.participantsAndPermission = []
+
+        untagged_key = Tag.getTagKeyByName("Untagged")
+        if not untagged_key:
+            untagged_key = Tag()
+            untagged_key.description = "Untagged"
+            untagged_key = Tag.addTagToDatastore(untagged_key)
+        budget.tagList.append(untagged_key)
+
+        for tag in tag_list.split(','):
+            if not tag:
+                break
+            tag_key = Tag.getTagKeyByName(tag)
+            if not tag_key:
+                self.response.write('Unrecognized tag ' + tag)
+                return
+            budget.tagList.append(tag_key)
+
+        for participant in participant_list.split(","):
+            budgeteer_name = participant.split(":")[0]
+            budgeteer_id = str(Budgeteer.getBudgeteerIdByUserName(budgeteer_name.lower()))
+            if not budgeteer_id:
+                self.response.write('No such username ' + budgeteer_name)
+                return
+            budgeteer_perm = participant.split(":")[1]
+            print budgeteer_perm
+            if "Manager" != budgeteer_perm and "Partner" != budgeteer_perm and "Viewer" !=  budgeteer_perm:
+                self.response.write('Unknown permission level ' + budgeteer_perm)
+                return
+            budget.participantsAndPermission.append(json.dumps({budgeteer_id : budgeteer_perm}))
+
+        for entry in budget.entryList:
+            temp_entry = Entry.getEntryByKey(entry)
+            tag_desc = Tag.getTagByKey(temp_entry.tagKey)
+            tag_desc = tag_desc.description
+            if tag_desc not in tag_list.split(','):
+                temp_entry.tagKey = Tag.getTagKeyByName("Untagged")
+                Entry.updateEntry(temp_entry)
+
+        Budget.addBudget(budget)
+        self.response.write(json.dumps({'status':'OK'}))
+
+
+app = webapp2.WSGIApplication([('/EditBudget/(.*)', EditBudgetHandler),
+                               ('/SubmitEditedBudget/(.*)', SubmitEditedBudgetHandler)
+                               ], debug=True)
